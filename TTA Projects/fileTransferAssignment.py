54dcca99f62faa0329619ff6b08216671b3b3404
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-03-25 12:43:48 -0600

    Create fileTransferAssignment.py
    Please take a look.

---
+++ TTA Projects/fileTransferAssignment.py
@@ -0,0 +1,69 @@
+# Set folder that has created/modified text files as source.
+# Set folder that is destination file as destination.
+# Iterate through files in source folder and check modification time.
+# If modification time <24hrs, move file to destination folder.
+
+# Will allow files to be moved
+import shutil
+
+# This will allow us to convert from epoch to local time.
+import time
+
+# Import os because we need os.listdir() which gets the list of all files and directories
+# in the specified directory.
+# We will also use os.path.getmtime() which will give us the modification time of the file
+# in epoch form where we'll have to convert for ability to check if <24 hours old.
+import os
+
+# Source folder where we'll check files and their modification times.
+source = 'D:/Users/bpoch/Desktop/FolderA/'
+
+# Destination folder where <24 hour modified files will be transferred to.
+destination = 'D:/Users/bpoch/Desktop/FolderB/'
+
+
+"""
+1) For this function, need to iterate through each file in source folder.
+2) For each file, check modification time.
+3) For each modification time, convert to <24 hour.
+4) If <24 hr mod time, move to destination folder.
+5) If >24 hr mod time, leave alone.
+"""
+def moveFile():
+    # Setting variable for .txt suffix only files.
+    txtSuffix = '.txt'
+    # Creating list of files from source file path.
+    files = os.listdir(source)
+    # Iterating through list to find .txt files only. Will go through all of them.
+    for file in files:
+        # If .txt is found, call on convertTime() and pass in that file.
+        if txtSuffix in file:
+            convertTime(file)
+            if qualModTime <= 24:
+                shutil.move(source + file, destination)
+            
+        
+
+def convertTime(file):
+    # Letting this function know path.
+    source = 'D:/Users/bpoch/Desktop/FolderA/'
+    # Returning mod time in epoch.
+    modTimeEpoch = os.path.getmtime('D:/Users/bpoch/Desktop/FolderA/' + file)
+    # Convert seconds since epoch to readable timestamp. We're only concerned about the
+    # past 24 hours.
+    qualModTime = time.strftime('%H', time.localtime(modTimeEpoch))
+    return qualModTime
+    
+    
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    moveFile()
+    
